48595b8273e4efc1c37b348c32fb9da95e2ecac1
David <david.katalinic9@gmail.com>
2023-08-06 23:53:43 +0200

    migrations

---
+++ database/migrations/2023_08_06_210225_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->unsignedInteger('quantity');
+            $table->boolean('status')->default(1);
+            $table->boolean('featured')->default(0);
+            $table->foreignId('brand_id')->constrained('brands')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
