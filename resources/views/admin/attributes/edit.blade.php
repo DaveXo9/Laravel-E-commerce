63b7cdbbda4ad4460e50dacbd206343e532ab9a9
David <david.katalinic9@gmail.com>
2023-08-16 23:47:18 +0200

    Attribute edit frontend only

--- resources/views/admin/attributes/edit.blade.php
+++ resources/views/admin/attributes/edit.blade.php
@@ -51,9 +51,90 @@
                         </form>
                     </div>
                 </div>
-                {{-- <div class="tab-pane" id="values">
-                    <attribute-values :attributeid="{{ $attribute->id }}"></attribute-values>
-                </div> --}}
+                <div class="tab-pane" id="values">
+                    <div class="tile">
+                        <h3 class="tile-title">Attribute Values</h3>
+                        <hr>
+                        <div class="tile-body">
+                            <div class="form-group">
+                                <label class="control-label" for="value">Value</label>
+                                <input
+                                    class="form-control"
+                                    type="text"
+                                    placeholder="Enter attribute value"
+                                    id="value"
+                                    name="value"
+                                />
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label" for="price">Price</label>
+                                <input
+                                    class="form-control"
+                                    type="number"
+                                    placeholder="Enter attribute value price"
+                                    id="price"
+                                    name="price"
+                                />
+                            </div>
+                        </div>
+                        <div class="tile-footer">
+                            <div class="row d-print-none mt-2">
+                                <div class="col-12 text-right">
+                                    <button class="btn btn-success" type="submit">
+                                        <i class="fa fa-fw fa-lg fa-check-circle"></i>Save
+                                    </button>
+                                    <button class="btn btn-success" type="submit" >
+                                        <i class="fa fa-fw fa-lg fa-check-circle"></i>Update
+                                    </button>
+                                    <button class="btn btn-primary" type="submit" >
+                                        <i class="fa fa-fw fa-lg fa-check-circle"></i>Reset
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                    <div id="">
+                        <div class="tile">
+                            <h3 class="tile-title">Option Values</h3>
+                            <div class="tile-body">
+                                <div class="table-responsive">
+                                    <table class="table table-sm">
+                                        <thead>
+                                        <tr class="text-center">
+                                            <th>#</th>
+                                            <th>Value</th>
+                                            <th>Price</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr v-for="value in values">
+                                            <td style="width: 25%" class="text-center">Place holder</td>
+                                            <td style="width: 25%" class="text-center">Place holder</td>
+                                            <td style="width: 25%" class="text-center">Place holder</td>
+                                            <td style="width: 25%" class="text-center">
+                                                <button class="btn btn-sm btn-primary">
+                                                    <i class="fa fa-edit"></i>
+                                                </button>
+                                                <button class="btn btn-sm btn-danger">
+                                                    <i class="fa fa-trash"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+                </div>
             </div>
         </div>      
     </div>
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -19,27 +19,11 @@
             </a>
         </li>
         <li class="treeview">
-            <a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-users"></i>
-                <span class="app-menu__label">Users</span>
-                <i class="treeview-indicator fa fa-angle-right"></i>
-            </a>
-
             <a class="app-menu__item {{ Route::currentRouteName() == 'admin.attributes.index' ? 'active' : '' }}" href="{{ route('admin.attributes.index') }}">
                 <i class="app-menu__icon fa fa-th"></i>
                 <span class="app-menu__label">Attributes</span>
             </a>
             
-            <ul class="treeview-menu">
-                <li>
-                    <a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Admin Users</a>
-                </li>
-                <li>
-                    <a class="treeview-item" href="#" target="_blank" rel="noopener noreferrer"><i class="icon fa fa-circle-o"></i> Roles</a>
-                </li>
-                <li>
-                    <a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Permissions</a>
-                </li>
-            </ul>
         </li>
                 <li>
                     <a class="app-menu__item {{ Route::currentRouteName() == 'admin.brands.index' ? 'active' : '' }}" href="{{ route('admin.brands.index') }}">
